4f80348ec84cf55ba6a2fa3f3d6d6a5208bb68c4
Glitch (glacier-feather) <none>
2017-12-07 19:29:52 +0000

    🎐📺 Checkpoint views/index.html:553280 server.js:553280 public/style.css:553280

--- server.js
+++ server.js
@@ -2,11 +2,9 @@
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use('/public', express.static(process.cwd() + '/public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
