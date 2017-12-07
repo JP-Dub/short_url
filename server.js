a9d1977fad0c91508787cb4da1502b292ed22d86
Glitch (glacier-feather) <none>
2017-12-07 20:02:46 +0000

    🍯📇 Checkpoint package.json:553280 views/index.html:553280 server.js:553280

--- server.js
+++ server.js
@@ -51,3 +51,29 @@ var dreams = [
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+/*
+app.use('/public', express.static(process.cwd() + '/public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+*/
