e4c39b7b1b6e28dd12d22c2376410eab731e8740
Glitch (glacier-feather) <none>
2017-12-07 21:52:30 +0000

    📼🏂 Checkpoint server.js:553280

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var express = require('express');
 var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGOLAB_URI;
 var app = express();
+var obj = {
+  "original" : "",
+  "shortened" : ""
+}
 
 // Use connect method to connect to the Server
   MongoClient.connect(url, function (err, db) {
@@ -27,22 +31,13 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/anyurl", function (req, res) {
-  res.send(JSON.stringify(req.body));
+app.get(":/anyurl", function (req, res) {
+  console.log(req.param)
+  obj.original = req.params;
+  obj.shortened = "null";
+  res.send(JSON.stringify(obj));
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/new", function (request, response) {
-  dreams.push(request.query.new);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
