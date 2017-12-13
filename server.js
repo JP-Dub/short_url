9cc92c45a5ad385f709671f1dab57cc27ca3f17a
Glitch (glacier-feather) <none>
2017-12-13 22:42:37 +0000

    🙄🍁 Checkpoint server.js:553280

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // init project
 var Mongo = require('mongodb').MongoClient;
 var express = require('express');
-//var isUrl = require('url');
+var validUrl = require('valid-url');
 var monurl = process.env.MONGOLAB_URI;
 var app = express();
 
@@ -29,16 +29,10 @@ Mongo.connect(monurl, function (err, db) {
  
   app.get("/*", function (req, res) {
     var url = req.params[0];
-    const { URL } = require('url');
-    var myURL; 
-  //new URL('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash');  
-    //console.log(myURL)
-    var reg = /_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS
-    if(!new URL(url)) {
-      console.log('no good');
-      //myURL = URL(url);
-      res.send(url + " is not a valid URL!");
-    } 
+    
+    if(!validUrl.isUri(url)) {
+      res.send(url + " this doesn't appear to be a valid URL!")
+    }
       
     // function to store and check db for url query and return results    
     function mapquest(url) {
