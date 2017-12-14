7be819f5a837c9819490d77bd63edc2bfd855e09
Glitch (glacier-feather) <none>
2017-12-14 22:15:55 +0000

    🐂👔 Checkpoint server.js:553280 mongod:553280

--- server.js
+++ server.js
@@ -30,23 +30,9 @@ Mongo.connect(mongoURL, function(err, db) {
   } else {
     console.log('Mongo connection established...');  
   }
-  
-  //var songs = db.insert('urlLib');
-  var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
- 
+  db.createCollection("urlLib");
+  var lib = db.collection("urlLib");
+
   app.get("/*", function (req, res, next) {
     var url = req.params[0];
     
