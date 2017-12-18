76f18d5817b906e93da017c79fe77dd137f5e0c6
Glitch (glacier-feather) <none>
2017-12-18 16:09:48 +0000

    🙇📹 Checkpoint server.js:553280

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ MongoClient.connect(mongoURL, function(err, db) {
   // Get the documents collection
   var collection = db.collection('documents');
   // Insert some documents
+    console.log(collection)
   collection.insertMany([
     {a : 1}, {a : 2}, {a : 3}
   ], function(err, result) {
@@ -42,6 +43,79 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+
+app.get("/*", function (req, res, next) {
+  var url = req.params[0];
+    
+    // checks if url query is a valid url
+  if(!validUrl.isUri(url)) {
+    var error = {};
+    error.Error = url + " doesn't appear to be a valid URL";
+    res.json(error);
+    return;
+  }
+      
+  // function to check, create, log, and post url queries and results.     
+  function mapquest(url) {
+      
+    // checks if urlLib is empty
+    var isEmpty = function() {
+      for(var key in urlLib) {
+        if(urlLib.hasOwnProperty(key)) 
+          return false;
+        } 
+      return true;
+    }
+    // posts result to the screen
+    var postData = function(log, url, short) {
+      if(log) {
+        urlLib[url] = short; //logs query to url library(urlLib)
+      }
+       data.obj.original_url = url;
+       data.obj.shortened_url = short;
+       res.json(data.obj);
+    }
+
+    // creates a random string to build the shortened url
+    var randomURL = function(z){
+      var short ="sho.rt/", str = data.str;
+      for (var i = 0; i < 6; i++) {
+        short += str[Math.floor(Math.random() * str.length)];
+      }
+        
+      //checks if short url address is in the db already
+      if(!isEmpty()) {
+        for(var key in urlLib) {
+          var val = urlLib[key];  
+          if (short === val) {
+            randomURL(z); // duplicate short url is found in the urlLib, creates a new string
+            return;
+          }              
+        }        
+        postData(true, z, short); // if no duplicates are found in the urlLib, post data
+        } else { 
+          postData(true, z, short); // if urlLib is empty, post data
+        }
+    } 
+      
+    //checks if  original url address is in the db already
+    if(!isEmpty()) {
+      for(var key in urlLib) {            
+        if (key === url) {
+         postData(false, url, urlLib[key]); // if duplicate url query is found in the urlLib, return the results
+         return;
+        }      
+      }
+      randomURL(url); // no duplicates are found      
+    } else {
+      randomURL(url); // the urlLib is empty
+    }
+  };
+
+mapquest(url);
+    
+});
+
 // Use connect method to connect to the Server
 MongoClient.connect(mongoURL, function(err, db) {
   if (err) {
@@ -50,80 +124,10 @@ MongoClient.connect(mongoURL, function(err, db) {
     console.log('Mongo connection established...');  
   }
 
-  //db.createCollection("urlLib");
-  //var lib = db.collection("urlLib");
+  
+  var lib = db.collection("urlLib");
 
-  app.get("/*", function (req, res, next) {
-    var url = req.params[0];
-    
-    // checks if url query is a valid url
-    if(!validUrl.isUri(url)) {
-      var error = {};
-      error.Error = url + " doesn't appear to be a valid URL";
-      res.json(error);
-      return;
-    }
-      
-    // function to check, create, log, and post url queries and results.     
-    function mapquest(url) {
-      
-      // checks if urlLib is empty
-      var isEmpty = function() {
-        for(var key in urlLib) {
-          if(urlLib.hasOwnProperty(key)) 
-            return false;
-          } 
-        return true;
-      }
-      // posts result to the screen
-      var postData = function(log, url, short) {
-        if(log) {
-          urlLib[url] = short; //logs query to url library(urlLib)
-        }
-         data.obj.original_url = url;
-         data.obj.shortened_url = short;
-         res.json(data.obj);
-      }
 
-      // creates a random string to build the shortened url
-      var randomURL = function(z){
-        var short ="sho.rt/", str = data.str;
-        for (var i = 0; i < 6; i++) {
-          short += str[Math.floor(Math.random() * str.length)];
-        }
-        
-        //checks if short url address is in the db already
-        if(!isEmpty()) {
-          for(var key in urlLib) {
-            var val = urlLib[key];  
-            if (short === val) {
-              randomURL(z); // duplicate short url is found in the urlLib, creates a new string
-              return;
-            }              
-          }        
-          postData(true, z, short); // if no duplicates are found in the urlLib, post data
-          } else { 
-            postData(true, z, short); // if urlLib is empty, post data
-          }
-      } 
-      
-      //checks if  original url address is in the db already
-      if(!isEmpty()) {
-        for(var key in urlLib) {            
-          if (key === url) {
-           postData(false, url, urlLib[key]); // if duplicate url query is found in the urlLib, return the results
-           return;
-          }      
-        }
-        randomURL(url); // no duplicates are found      
-      } else {
-        randomURL(url); // the urlLib is empty
-      }
-    };
-
-  mapquest(url);
-    
-  });
   //Close connection
   db.close();
   
